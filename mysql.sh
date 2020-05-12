ef093ad84e49a88638249f9f5e326982719c1620
Дмитрий <polishuk1003@gmail.com>
2020-05-12 22:36:48 +0300

    fix

--- mysql.sh
+++ mysql.sh
@@ -4,10 +4,10 @@ if ! [[ "$1" ]]; then
     exit
 fi
 sudo apt update -y && sudo apt upgrade -y
-sudo apt-get install mariadb-server
-sudo apt-get install default-libmysqlclient-dev
-sudo apt-get install libqt5sql5-mysql
-sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils
+sudo apt-get install mariadb-server -y
+sudo apt-get install default-libmysqlclient-dev -y
+sudo apt-get install libqt5sql5-mysql -y
+sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils -y
 sudo mysql -u root
 UPDATE mysql.user SET plugin = 'mysql_native_password', Password = PASSWORD\('$1'\) WHERE User = 'root';
 FLUSH PRIVILEGES;
